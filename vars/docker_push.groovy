ac129aef0b22364428dbace123216646e185df3b
rahoolch <112589734+rahoolch@users.noreply.github.com>
2025-03-23 14:17:13 -0400

    indentation updated

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,6 @@
 def call(String Project, String NameTag){
   withCredentials([usernamePassword(credentialsId: "dockerHubCred", passwordVariable: "dockerHubPass", usernameVariable:"dockerHubUser")]){
-                    sh "docker login -u ${dockerHubUser} -p ${dockerHubPass} "
-                    
-                }
+    sh "docker login -u ${dockerHubUser} -p ${dockerHubPass} "
+  }
   sh "docker push ${dockerHubUser}/${Project}:${NameTag}"
 }
