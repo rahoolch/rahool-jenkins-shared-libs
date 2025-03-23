7c63aa8c6d065a29ef41acccc53dd5851c06fac7
rahoolch <112589734+rahoolch@users.noreply.github.com>
2025-03-23 14:14:48 -0400

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,7 @@
+def call(String Project, String NameTag){
+  withCredentials([usernamePassword(credentialsId: "dockerHubCred", passwordVariable: "dockerHubPass", usernameVariable:"dockerHubUser")]){
+                    sh "docker login -u ${dockerHubUser} -p ${dockerHubPass} "
+                    
+                }
+  sh "docker push ${env.dockerHubUser}/${Project}:${NameTag}"
+}
